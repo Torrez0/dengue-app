b8d8a327e234a06f653bda77c6140a7c54e03d98
unknown <eduardo.naganolacerda@gmail.com>
2024-05-23 23:31:12 -0300

    Adicionado o FireBase ao projeto

---
+++ src/config/firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import {initializeAuth} from "firebase/auth/reac-native"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7Nn1KRNvrO7Qi9Br_2SOf0kk3ERow9fs",
+  authDomain: "pi-dengue.firebaseapp.com",
+  projectId: "pi-dengue",
+  storageBucket: "pi-dengue.appspot.com",
+  messagingSenderId: "481530595582",
+  appId: "1:481530595582:web:179c084095fae4fc753daf",
+  measurementId: "G-Z8ZNNVM09B"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(AsyncStorage)
+})
+
+export {auth};
